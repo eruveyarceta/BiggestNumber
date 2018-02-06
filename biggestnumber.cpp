5e8d6c3b9f1c44f6c82cfc72922eba59252c2973
eruveyarceta <earceta@mail.csuchico.edu>
2018-02-06 11:55:27 -0800

    Biggest Number Lab

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,12 +5,20 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+    int a, b, biggest;
 
-  cout<<"Of those two numbers, the biggest is: ";
+  cout<<"Please enter a whole number:\n";
+    cin>> a;  
+  cout<<"Please enter another whole number:\n";
+    cin>> b;
+
+    if(a>b)
+        { biggest=a;
+        }
+    else{
+            biggest=b;
+            }
+  cout<<"Of those two numbers, the biggest is: "<<biggest;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
